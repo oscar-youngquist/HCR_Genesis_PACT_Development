cdd708cb78e54d217f89107ee2934f0b75c93981
oscar-youngquist <oscar.youngquist@gmail.com>
2026-04-14 11:15:21 -0400

    minor change

--- legged_gym/scripts/go2_pact_pos.sh
+++ legged_gym/scripts/go2_pact_pos.sh
@@ -6,4 +6,4 @@ conda activate /home/oyoungquist/.conda/envs/genesis_lr
 
 export SIMULATOR=genesis_pact_pos
 
-python train.py --task=go2_pact_pos --headless --gpu=cuda:1
+python train.py --task=go2_pact_pos --headless
