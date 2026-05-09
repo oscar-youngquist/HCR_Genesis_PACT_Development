55134a266b90b42a1ee299cc8b3596db1afa83be
Oscar Youngquist <oscar.youngquist@gmail.com>
2026-05-09 18:15:09 -0400

    legion local training changes sync

--- legged_gym/scripts/go1_tau.sh
+++ legged_gym/scripts/go1_tau.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-. /home/oyoungquist/anaconda3/etc/profile.d/conda.sh
+. /home/oscaryoungquist/anaconda3/etc/profile.d/conda.sh
 
-conda activate /home/oyoungquist/.conda/envs/genesis_lr
+conda activate /home/oscaryoungquist/.conda/envs/genesis_lr
 
 export SIMULATOR=genesis_pact_postau
 
-python train.py --task=go1_tau --headless --gpu=cuda:1 --seed=1
+python train.py --task=go1_tau --headless --seed=1
