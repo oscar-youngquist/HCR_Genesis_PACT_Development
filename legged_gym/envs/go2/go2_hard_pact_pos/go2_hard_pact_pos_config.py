6f45cf65411c70db0335bbbf7f001c94a06a0792
oscar-youngquist <oscar.youngquist@gmail.com>
2026-09-04 15:15:48 -0400

    small change

--- legged_gym/envs/go2/go2_hard_pact_pos/go2_hard_pact_pos_config.py
+++ legged_gym/envs/go2/go2_hard_pact_pos/go2_hard_pact_pos_config.py
@@ -453,7 +453,7 @@ class GO2HardPACTPosCfgPPO(LeggedRobotCfgPPO):
         # Shared by the context encoder and all auxiliary decoder heads.
         auxiliary_learning_rate = 0.0002
         # Weight beta on the latent KL term in the combined auxiliary loss.
-        vae_kld_weight = 0.01
+        vae_kld_weight = 0.1
         vae_kl_initial_weight = 0.0001
         vae_kl_warmup_start = 0
         vae_kl_warmup_iterations = 1000
