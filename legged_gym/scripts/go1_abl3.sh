a4ca4ca8727079df7710221279d89fcd0a6118c9
pghezzi <100241979+pghezzi@users.noreply.github.com>
2026-09-10 23:20:21 -0400

    updates

--- legged_gym/scripts/go1_abl3.sh
+++ legged_gym/scripts/go1_abl3.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-. /home/oyoungquist/anaconda3/etc/profile.d/conda.sh
+###
+#   Bizon
+###
+# . /home/oyoungquist/anaconda3/etc/profile.d/conda.sh
 
-conda activate /home/oyoungquist/.conda/envs/genesis_lr
+# conda activate /home/oyoungquist/.conda/envs/genesis_lr
+
+###
+#   Omen
+###
+. /home/oscaryoungquist/anaconda3/etc/profile.d/conda.sh
+
+conda activate /home/oscaryoungquist/anaconda3/envs/genesis_lr
 
 export SIMULATOR=genesis_pact_nopinn
 
-python train.py --task=go1_abl3 --headless --gpu=cuda:1 --seed=1
+python train.py --task=go1_abl3 --headless --seed=1
